40b3b56272010c5a020e03ff559cdadd06e6c4ec
dingbean@126.com <50908050c>
2016-09-18 15:39:11 +0800

    add

---
+++ wechat/src/main/java/cn/sh/outer/controller/TestController.java
@@ -0,0 +1,27 @@
+package cn.sh.outer.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by Administrator on 2016/9/18.
+ */
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    private static Logger log = LoggerFactory.getLogger(TestController.class);
+
+    @RequestMapping(value = "/controller", method= RequestMethod.GET)
+    @ResponseBody
+    public void upload(HttpServletRequest request){
+        log.info("============================");
+        System.out.println("###################");
+    }
+}
