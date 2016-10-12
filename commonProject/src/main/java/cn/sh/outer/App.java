dda4ea80d593f54a798c19cf9235a8974b9c34f2
dingbean@126.com <50908050c>
2016-10-12 10:22:31 +0800

    add

---
+++ commonProject/src/main/java/cn/sh/outer/App.java
@@ -0,0 +1,13 @@
+package cn.sh.outer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
